075174673ed817cb38cd5eae9efe5a22b593e157
Luca Negrette <lucanegrette@hotmail.com>
2019-11-09 18:30:40 -0300

    csca

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -11,7 +11,6 @@ class Cart extends Model
 {   
 
     const SESSION = "Cart";
-	const SESSION_ERROR = "CartError";
 
 	public static function getFromSession()
 	{
@@ -20,23 +19,27 @@ class Cart extends Model
         // var_dump( $_SESSION[Cart::SESSION] );
         // var_dump( (int)$_SESSION[Cart::SESSION]['idcart'] );
         // exit;
-		if (isset($_SESSION[Cart::SESSION]) && (int)$_SESSION[Cart::SESSION]['idcart'] > 0) {
+        if (isset($_SESSION[Cart::SESSION]) && (int)$_SESSION[Cart::SESSION]['idcart'] > 0) 
+        {
 
 			$cart->get((int)$_SESSION[Cart::SESSION]['idcart']);
 
-		} else {
+        } else 
+        {
 
 			$cart->getFromSessionID();
 
-			if (!(int)$cart->getidcart() > 0) {
+            if (!(int)$cart->getidcart() > 0) 
+            {
 
 				$data = [
 					'dessessionid'=>session_id()
 				];
 
-				if (User::checkLogin(false)) {
+                if (User::checkLogin(false)) 
+                {
 
-					$user = User::getFromSession();
+					$user = User::getFromSession(); // DA CLASSE USER
 					
 					$data['iduser'] = $user->getiduser();	
 
@@ -73,7 +76,8 @@ class Cart extends Model
 			':dessessionid'=>session_id()
 		]);
 
-		if (count($results) > 0) {
+        if (count($results) > 0) 
+        {
 
 			$this->setData($results[0]);
 
@@ -114,104 +118,58 @@ class Cart extends Model
 
 		$this->setData($results[0]);
 
-	}
+    }
+    
+    public function addProduct(Product $product)
+    {
+        $sql = new Sql();
+        $sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES(:idcart, :idproduct)", [
+            ":idcart"=>$this-getidcart(),
+            ":idproduct"=>$product->getidproduct()
+        ]);
+    }
 
+    public function removeProduct(Product $product, $all = false)
+    {
+        $sql = new Sql();
 
-        // const SESSION = "Cart";
+        if ($all)
+        {
+        $sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", [
+            ":idcart"=>$this-getidcart(),
+            ":idproduct"=>$product->getidproduct()
+        ]);
 
-        // public static function getFromSession()
-        // {
-        //     $cart = new Cart();
+        } else
+        {
+            $sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", [
+                ":idcart"=>$this-getidcart(),
+                ":idproduct"=>$product->getidproduct()
+            ]);
+        }
+    }
 
-        //     if (isset($_SESSION[Cart::SESSION]) && (int)$_SESSION[Cart::SESSION]['idcart'] > 0)
-        //     {
-        //         $cart->get((int)$_SESSION[Cart::SESSION]['idcart']);
-        //         var_dump($cart->get((int)$_SESSION[Cart::SESSION]['idcart']));
-        //         exit;
-        //     }
-        //     else
-        //     {
-        //         $cart->getFromSessionID();
+    public function getProducts()
+    {
+        $sql = new Sql();
 
-        //         if (!(int)$cart->getidcart() > 0) 
-        //         {
-        //             $data = [
-        //                 "dessessionid"=>session_id()
-        //             ];
-
-        //             if (User::checkLogin(false))
-        //             {
-        //                 $user = User::getFromSession();
-
-        //                 $data['iduser'] = $user->getiduser();
-
-        //             }
-
-        //             $cart->setData($data);
-        //             $cart->save();
-
-        //             $cart->setToSession();
-
-                    
-        //         }
-
-        //     }
-
-        //     return $cart;
-        // }
-
-        // public function setToSession()
-        // {
-        //     $_SESSION[Cart::SESSION] = $this->getValues();
-        // }
-
+        $rows = $sql->select("
+        SELECT b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal 
+        FROM tb_cartsproducts a 
+        INNER JOIN tb_products b ON a.idproduct = b.idproduct 
+        WHERE a.idcart = :idcart AND a.dtremoved IS NULL 
+        GROUP BY b.idproduct, b.desproduct , b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl 
+        ORDER BY b.desproduct
         
-        // public function getFromSessionID()
-        // {
-        //     $sql = new Sql();
-        //     $results = $sql->select("SELECT * FROM tb_carts WHERE dessessionid = :dessessionid", [
-        //         ":dessessionid"=>session_id()
-        //     ]);
+        
+        ", [
+            ":idcart"=>$this->getidcart()
+        ]);
+        return Product::checkList($rows);
+    }
 
-        //     if (count($results) > 0)
-        //     {
-        //         $this->setData($results[0]);
 
-        //     }
-
-        // }
-
-        // public function get(int $idcart)
-        // {
-        //     $sql = new Sql();
-        //     $results = $sql->select("SELECT * FROM tb_carts WHERE idcart = :idcart", [
-        //         ":idcart"=>$idcart
-        //     ]);
-
-        //     if (count($results) > 0)
-        //     {
-        //         $this->setData($results[0]);
-
-        //     }
-
-        // }
-
-        // public function save()
-        // {
-        //     $sql = new Sql();
-        //     $results = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduser, :idaddress, :vlfreight, :nrdays)", [
-        //         ":idcart"=>$this->getidcart(),
-        //         ":dessessionid"=>$this->getdessessionid(),
-        //         ":iduser"=>$this->getiduser(),
-        //         ":idaddress"=>$this->getidaddress(),
-        //         ":vlfreight"=>$this->getvlfreight(),
-        //         ":nrdays"=>$this->getnrdays()
-        //     ]);
-        //     var_dump($results);
-        //     exit;
-
-        //     $this->setData($results[0]);
-        // }
+       
 
 }
 
