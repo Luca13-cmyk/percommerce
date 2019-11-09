111bd840629f22e5ee39c374a85b19a22908e62a
Luca Negrette <lucanegrette@hotmail.com>
2019-11-09 16:27:41 -0300

    csac

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -98,6 +98,8 @@ class Cart extends Model
                 ":vlfreight"=>$this->getvlfreight(),
                 ":nrdays"=>$this->getnrdays()
             ]);
+            var_dump($results[0]);
+            exit;
 
             $this->setData($results[0]);
         }
