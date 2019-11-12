16fa2678315f272711d21b01336242ab13c4081c
Luca Negrette <lucanegrette@hotmail.com>
2019-11-12 17:18:43 -0300

    cscac

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -127,7 +127,7 @@ class User extends Model {
 		$sql = new Sql();
 		
 		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
-			":desperson"=>$this->utf8_decode($this->getdesperson()),
+			":desperson"=>utf8_decode($this->getdesperson()),
 			":deslogin"=>$this->getdeslogin(),
 			":despassword"=>User::getPasswordHash($this->getdespassword()),
 			":desemail"=>$this->getdesemail(),
