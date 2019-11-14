28c004b1f88afc3a8738ccae277ef3df6d23aed8
Luca Negrette <lucanegrette@hotmail.com>
2019-11-14 18:54:59 -0300

    scacas

--- site.php
+++ site.php
@@ -152,20 +152,32 @@ $app->get("/checkout", function() {
     User::verifyLogin(false, false);
     $cart = Cart::getFromSession();
     $address = new Address();
-    
+    if (!isset($_GET["zipcode"]))
+    {
+        $_GET["zipcode"] = $cart->getidaddress();
+    }
 
     if (isset($_GET["zipcode"]))
     {
         // echo  json_encode($address->loadFromCEP($_GET["zipcode"]));
         // exit;
         $address->loadFromCEP($cart->getidaddress());
-        
+
         $cart->setidaddress($_GET["zipcode"]);
 
         $cart->save();
 
         $cart->getCalculateTotal();
     }
+
+    if (!$address->getdesaddress()) $address->setdesaddress('');
+	if (!$address->getdesnumber()) $address->setdesnumber('');
+	if (!$address->getdescomplement()) $address->setdescomplement('');
+	if (!$address->getdesdistrict()) $address->setdesdistrict('');
+	if (!$address->getdescity()) $address->setdescity('');
+	if (!$address->getdesstate()) $address->setdesstate('');
+	if (!$address->getdescountry()) $address->setdescountry('');
+	if (!$address->getdeszipcode()) $address->setdeszipcode('');
 
     
     
@@ -181,7 +193,8 @@ $app->get("/checkout", function() {
     $page->setTpl("checkout", [
         "cart"=>$cart->getValues(),
         "address"=>$address->getValues(),
-        "products"=>$cart->getProducts()
+        "products"=>$cart->getProducts(),
+        "error"=>Address::getMsgError()
         
     ]);
 
@@ -189,6 +202,59 @@ $app->get("/checkout", function() {
 
 $app->post("/checkout", function() {
 
+
+    User::verifyLogin(false, false);
+
+    if(!isset($_POST["zipcode"]) || $_POST["zipcode"] === "")
+    {
+        Address::setMsgError("Informe o CEP");
+        header("Location: /checkout");
+        exit;
+    }
+    if(!isset($_POST["desaddress"]) || $_POST["desaddress"] === "")
+    {
+        Address::setMsgError("Informe o Endereco");
+        header("Location: /checkout");
+        exit;
+    }
+    if(!isset($_POST["desdistrict"]) || $_POST["desdistrict"] === "")
+    {
+        Address::setMsgError("Informe o bairro");
+        header("Location: /checkout");
+        exit;
+    }
+    if(!isset($_POST["descity"]) || $_POST["descity"] === "")
+    {
+        Address::setMsgError("Informe a Cidade");
+        header("Location: /checkout");
+        exit;
+    }
+    if(!isset($_POST["desstate"]) || $_POST["desstate"] === "")
+    {
+        Address::setMsgError("Informe o Estado");
+        header("Location: /checkout");
+        exit;
+    }
+    if(!isset($_POST["descountry"]) || $_POST["descountry"] === "")
+    {
+        Address::setMsgError("Informe o pais");
+        header("Location: /checkout");
+        exit;
+    }
+
+    $user = User::getFromSession();
+
+    $address = new Address();
+
+    $_POST["deszipcode"] = $_POST["zipcode"];
+    $_POST["idperson"] = $user->getidperson();
+
+    $address->setData($_POST);
+
+    $address->save();
+
+    header("Location: /order");
+    
 
 
 });
--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -8,6 +8,8 @@ use \Hcode\Model;
 class Address extends Model 
 {   
 
+        const SESSION_ERROR = "AddressError";
+
        public static function getCEP($nrcep)
        {
            $nrcep = str_replace("-", "", $nrcep);
@@ -42,13 +44,51 @@ class Address extends Model
             $this->setdescity($data["localidade"]);
             $this->setdesstate($data["uf"]);
             $this->setdescountry("Brasil");
-            $this->setnrzipcode($nrcep);
+            $this->setdeszipcode($nrcep);
 
         }
         return parent::getValues();
 
 
        }
+
+       public function save()
+       {
+           $sql = new Sql();
+           $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :desnumber, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
+			':idaddress'=>$this->getidaddress(),
+			':idperson'=>$this->getidperson(),
+			':desaddress'=>utf8_decode($this->getdesaddress()),
+			':desnumber'=>$this->getdesnumber(),
+			':descomplement'=>utf8_decode($this->getdescomplement()),
+			':descity'=>utf8_decode($this->getdescity()),
+			':desstate'=>utf8_decode($this->getdesstate()),
+			':descountry'=>utf8_decode($this->getdescountry()),
+			':deszipcode'=>$this->getdeszipcode(),
+			':desdistrict'=>$this->getdesdistrict()
+        ]);
+        if (count($results) > 0)
+        {
+            $this->setData($results[0]);
+        }
+     }
+     public static function setMsgError($msg)
+     {
+         $_SESSION[Address::SESSION_ERROR] = $msg;
+     }
+ 
+     public static function getMsgError()
+     {
+         $msg = (isset($_SESSION[Address::SESSION_ERROR])) ? $_SESSION[Address::SESSION_ERROR] : "";
+ 
+         Address::clearMsgError();
+ 
+         return $msg;
+     }
+     public static function clearMsgError()
+     {
+         $_SESSION[Address::SESSION_ERROR] = NULL;
+     }
 
 
 
