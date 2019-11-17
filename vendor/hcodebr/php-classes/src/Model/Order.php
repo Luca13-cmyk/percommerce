8c5777c907d778d91df8df1c0d4ced923821ee85
Luca Negrette <lucanegrette@hotmail.com>
2019-11-17 18:26:41 -0300

    casca

--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -21,7 +21,7 @@ class Order extends Model
             
         ]);
 
-        if (count($results > 0))
+        if (count($results) > 0)
         {
             $this->setData($results[0]);
         }
